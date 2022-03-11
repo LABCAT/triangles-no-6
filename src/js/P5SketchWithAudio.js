0c0f5208b48a920ce0881da72019470d8a220f84
LABCAT <shane.watters.nz@gmail.com>
2022-03-11 18:55:54 +1300

    Audio selection and repo setup.

---
+++ src/js/P5SketchWithAudio.js
@@ -0,0 +1,154 @@
+import React, { useRef, useEffect } from "react";
+import "./helpers/Globals";
+import "p5/lib/addons/p5.sound";
+import * as p5 from "p5";
+import { Midi } from '@tonejs/midi'
+import PlayIcon from './functions/PlayIcon.js';
+
+import audio from "../audio/triangles-no-6.ogg";
+import midi from "../audio/triangles-no-6.mid";
+
+const P5SketchWithAudio = () => {
+    const sketchRef = useRef();
+
+    const Sketch = p => {
+
+        p.canvas = null;
+
+        p.canvasWidth = window.innerWidth;
+
+        p.canvasHeight = window.innerHeight;
+
+        p.audioLoaded = false;
+
+        p.player = null;
+
+        p.PPQ = 3840 * 4;
+
+        p.loadMidi = () => {
+            Midi.fromUrl(midi).then(
+                function(result) {
+                    const noteSet1 = result.tracks[5].notes; // Synth 1
+                    p.scheduleCueSet(noteSet1, 'executeCueSet1');
+                    p.audioLoaded = true;
+                    document.getElementById("loader").classList.add("loading--complete");
+                    document.getElementById("play-icon").classList.remove("fade-out");
+                }
+            );
+            
+        }
+
+        p.preload = () => {
+            p.song = p.loadSound(audio, p.loadMidi);
+            p.song.onended(p.logCredits);
+        }
+
+        p.scheduleCueSet = (noteSet, callbackName, poly = false)  => {
+            let lastTicks = -1,
+                currentCue = 1;
+            for (let i = 0; i < noteSet.length; i++) {
+                const note = noteSet[i],
+                    { ticks, time } = note;
+                if(ticks !== lastTicks || poly){
+                    note.currentCue = currentCue;
+                    p.song.addCue(time, p[callbackName], note);
+                    lastTicks = ticks;
+                    currentCue++;
+                }
+            }
+        } 
+
+        p.setup = () => {
+            p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
+            p.background(0);
+        }
+
+        p.draw = () => {
+            if(p.audioLoaded && p.song.isPlaying()){
+
+            }
+        }
+
+        p.executeCueSet1 = (note) => {
+            p.background(p.random(255), p.random(255), p.random(255));
+            p.fill(p.random(255), p.random(255), p.random(255));
+            p.noStroke();
+            p.ellipse(p.width / 2, p.height / 2, p.width / 4, p.width / 4);
+        }
+
+        p.mousePressed = () => {
+            if(p.audioLoaded){
+                if (p.song.isPlaying()) {
+                    p.song.pause();
+                } else {
+                    if (parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)) {
+                        p.reset();
+                    }
+                    document.getElementById("play-icon").classList.add("fade-out");
+                    p.canvas.addClass("fade-in");
+                    p.song.play();
+                }
+            }
+        }
+
+        p.creditsLogged = false;
+
+        p.logCredits = () => {
+            if (
+                !p.creditsLogged &&
+                parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)
+            ) {
+                p.creditsLogged = true;
+                    console.log(
+                    "Music By: http://labcat.nz/",
+                    "\n",
+                    "Animation By: https://github.com/LABCAT/"
+                );
+                p.song.stop();
+            }
+        };
+
+        p.reset = () => {
+
+        }
+
+        p.updateCanvasDimensions = () => {
+            p.canvasWidth = window.innerWidth;
+            p.canvasHeight = window.innerHeight;
+            p.canvas = p.resizeCanvas(p.canvasWidth, p.canvasHeight);
+        }
+
+        if (window.attachEvent) {
+            window.attachEvent(
+                'onresize',
+                function () {
+                    p.updateCanvasDimensions();
+                }
+            );
+        }
+        else if (window.addEventListener) {
+            window.addEventListener(
+                'resize',
+                function () {
+                    p.updateCanvasDimensions();
+                },
+                true
+            );
+        }
+        else {
+            //The browser does not support Javascript event binding
+        }
+    };
+
+    useEffect(() => {
+        new p5(Sketch, sketchRef.current);
+    }, []);
+
+    return (
+        <div ref={sketchRef}>
+            <PlayIcon />
+        </div>
+    );
+};
+
+export default P5SketchWithAudio;
